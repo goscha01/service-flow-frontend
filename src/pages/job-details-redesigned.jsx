e70e4c2fbf82cef5aad8548f038d3db61ded42ac
Weblord <68680206+bopmack@users.noreply.github.com>
2025-12-11 08:58:03 -0400

    dimt

--- src/pages/job-details-redesigned.jsx
+++ src/pages/job-details-redesigned.jsx
@@ -4295,9 +4295,9 @@ const JobDetails = () => {
           </div>
         )}
 
-        {/* Edit Service Modal */}
-        {showEditServiceModal && (
-          <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-[9999] p-4">
+    {/* Edit Service Modal */}
+    {showEditServiceModal && (
+      <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-[9999] p-4">
             <div className="bg-white rounded-lg shadow-xl max-w-4xl w-full max-h-[90vh] flex flex-col">
               <div className="p-6 flex-1 overflow-y-auto">
                 <div className="flex items-center justify-between mb-6">
